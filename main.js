453aaa84dd81447079d746403cd523cc66fde6ae
Emily Morgan <emorgan921@gmail.com>
2024-08-05 16:19:35 -0400

    Deconstructing props, lists and mapping in React.

--- main.js
+++ main.js
@@ -1,19 +1,86 @@
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+//for loop
+	// const numbers = [1, 2, 3, 4, 5];
+	// const tens = [];
 
-function renderElement() {
-  const element = (
-    <div className="post">
-      <h1>My First Blog Post</h1>
-      <div>Author: Mark Twain</div>
-      <div>Published: {new Date().toLocaleTimeString()}</div>
-      <p>
-        I am new to blogging and this is my first post. You should expect a
-        lot of great things from me.
-      </p>
-    </div>
-  );
+	// for (let index = 0; index < numbers.length; index++) {
+	//   const number = numbers[index];
+	//   tens.push(number * 10);
+	// }
 
-  root.render(element);
+	// console.log(tens);
+
+//array forEach
+	// const numbers = [1, 2, 3, 4, 5];
+	// const tens = [];
+
+	// numbers.forEach(function (number) {
+	// tens.push(number * 10);
+	// });
+
+	// console.log(tens);
+
+//#array.map
+	// const numbers = [1, 2, 3, 4, 5]; //this will return the same amount of items as you put in
+
+	// const tens = numbers.map(function (number) {
+	// return number * 10;
+	// });
+
+	// console.log(tens);
+
+//#array.map with arrow function
+	// const numbers = [1, 2, 3, 4, 5];
+	// const tens = numbers.map((number) => number * 10); //because the function only has one line, you dont need {} and "return" is implied. 
+	// console.log(tens);
+
+//how do we do this in REACT? an array of jsx elements and place into unordered list:
+// function FruitList(props) {
+// 	const fruitListItems = props.fruits.map((fruit) => (
+// 	  <li key={fruit.id}>{fruit.name}</li>
+// 	));
+// 	return <ul>{fruitListItems}</ul>;
+//    }
+   
+//    const data = [
+// 	{ id: 1, name: 'apple' },
+// 	{ id: 2, name: 'orange' },
+// 	{ id: 3, name: 'blueberry' },
+// 	{ id: 4, name: 'banana' },
+// 	{ id: 5, name: 'kiwi' },
+//    ];
+   
+//    ReactDOM.createRoot(document.getElementById('root')).render(
+// 	<FruitList fruits={data} />
+//    );
+
+function FruitListCard({fruit}) {
+	return (
+	<div>
+		<li> {fruit.id}. {fruit.name} {fruit.color} </li>;
+	</div>
+	)
 }
-setInterval(renderElement, 1000);
+
+function FruitList(props) {
+	const fruitListItems = props.fruits.map((fruit) => ( //loop
+	  <FruitListItem key={fruit.id} fruit={fruit}/> //invented a new HTML tag with attribute named fruit equalling fruit
+	));
+	return <ul>{fruitListItems}</ul>;
+   }
+   
+   const data = [
+	{ id: 1, name: 'apple', color: "red" },
+	{ id: 2, name: 'orange', color: "orange" },
+	{ id: 3, name: 'blueberry', color: "purple" },
+	{ id: 4, name: 'banana', color: "yellow" },
+	{ id: 5, name: 'kiwi', color: "brown" },
+   ];
+   
+   ReactDOM.createRoot(document.getElementById('root')).render(
+	<FruitList fruits={data} />
+   );
+
+
+
+
+
---
+++ main1.js
@@ -0,0 +1,20 @@
+// let person = { first: "Shane", last: "Holman" };
+
+// function Greeter({ first, last }) {
+// 	//props.first = "Dave";		 cannot update props, they are read only.
+
+// 	// const first = props.first;
+// 	// const last = props.last;   	can combine these into one line of code and remove props. from h1 tag in return.
+
+// 	//const { first, last } = props; 	further, you can remove this and palce the prop names in the function parenthesis
+// 	return (
+// 		<h1>
+// 			Hello {first} {last}
+// 		</h1>
+// 	);
+// }
+// ReactDOM.createRoot(document.getElementById("root")).render(<Greeter first={person.first} last={person.last} />);
+
+// //react is doing the below when react is called
+// //let properties = {first: Eric}
+// //Greeter(properties);
