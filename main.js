4f37796a033898c960e301421c614a9f4bc6cd6c
Emily Morgan <emorgan921@gmail.com>
2024-08-06 11:45:15 -0400

    adding event and encasing onClick event in a function to pass parameters.

---
+++ main.js
@@ -0,0 +1,33 @@
+// //Events, generally user interaction triggers function.
+// function handleClick() {
+// 	console.log("clicked"); //console only
+// }
+
+// function App() {
+// 	return <button onClick={handleClick}>Click Me!</button>; //remove the parenthesis when calling the function otherwise it will automatically run the function.
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+function FruitListItem(props) {
+	function handleClick(id) {
+		console.log(`removed ${id}`);
+	}
+	// return <li onClick={handleClick(props.fruit.id)}>{props.fruit.name} </li>;
+	return <li onClick={() => handleClick(props.fruit.id)}>{props.fruit.name} </li>;
+}
+
+function FruitList(props) {
+	const fruitListItems = props.fruits.map((fruit) => <FruitListItem key={fruit.id} fruit={fruit} />);
+	return <ul>{fruitListItems}</ul>;
+}
+
+const data = [
+	{ id: 1, name: "apple" },
+	{ id: 2, name: "orange" },
+	{ id: 3, name: "blueberry" },
+	{ id: 4, name: "banana" },
+	{ id: 5, name: "kiwi" },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(<FruitList fruits={data} />);
